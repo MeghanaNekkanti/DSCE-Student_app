f773d157b03c5c7a86090803a9a38d8de849161d
Rohan Kumar <rohan.carmel@gmail.com>
2016-08-01 00:59:06 +0530

    nav drawer changes

--- app/src/main/java/com/example/meghana/testing/NewsFeedActivity.java
+++ app/src/main/java/com/example/meghana/testing/NewsFeedActivity.java
@@ -1,6 +1,10 @@
 package com.example.meghana.testing;
 
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
@@ -178,6 +182,33 @@ public class NewsFeedActivity extends AppCompatActivity
             Toast.makeText(NewsFeedActivity.this, "Yet to be implemented", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.edit_profile) {
             Toast.makeText(NewsFeedActivity.this, "Yet to be implemented", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_rate) {
+            Uri uri = Uri.parse("market://details?id=" + getBaseContext().getPackageName());
+            Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+            // To count with Play market backstack, After pressing back button,
+            // to taken back to our application, we need to add following flags to intent.
+            goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                    Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                    Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+            try {
+                startActivity(goToMarket);
+            } catch (ActivityNotFoundException e) {
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("http://play.google.com/store/apps/details?id=" + getBaseContext().getPackageName())));
+            }
+        } else if (id == R.id.nav_share) {
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT, "Download the app at http://play.google.com/store/apps/details?id=" + getBaseContext().getPackageName() + " to receive notifications.");
+            sendIntent.setType("text/plain");
+            startActivity(Intent.createChooser(sendIntent, "DSCE Notify"));
+        }
+        else if (id == R.id.nav_about){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(NewsFeedActivity.this);
+            alertDialog.setTitle("DSCE Notify About");
+            alertDialog.setMessage("App developed by Meghana Nekkanti (CSE,DSCE)");
+            alertDialog.setPositiveButton("OK",null);
+            alertDialog.show();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
